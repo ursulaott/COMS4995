f24a3730f9071247dc71e85b1185f94092008bf0
ursula <uso2000@columbia.edu>
2020-10-12 20:07:10 -1000

    fix

--- tests/test5.py
+++ tests/test5.py
@@ -1,11 +1,11 @@
 # test trying to open a path that does not contain an image
 # expect to get
 # No such file or directory: '../examples/nonexistient.jpg'
+# filenotfounderror is not defined in python2?
 
 import drkrm
 import pytest
 
 def test_path_empty():
-    with pytest.raises(FileNotFoundError) as e:
+    with pytest.raises(FileNotFoundError):
         drkrm.read_img("../examples/nonexistient.jpg")
-        print(e)
