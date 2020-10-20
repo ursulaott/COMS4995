c9f998a40bc71de4580fce0f6285f9be5260aaa2
ursula <uso2000@columbia.edu>
2020-10-20 13:32:54 -1000

    add flake8 linting

--- src/drkrm.py
+++ src/drkrm.py
@@ -4,10 +4,15 @@ def get_path():
     path = "../examples/kakashi.jpg"
     return path
 
+
 def get_input_path():
     upath = input("enter image path: ")
     return upath
 
+
 def read_img(imgpath):
     im = Image.open(imgpath)
     im.show()
+
+
+# to appease flake8
