0b70f7b24f0204c49a9173a0b4c7864bbb676685
ursula <uso2000@columbia.edu>
2020-11-05 13:21:43 -1000

    rtd link in readme

--- docs/conf.py
+++ docs/conf.py
@@ -4,7 +4,7 @@ html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 
-
+master_doc=["index"]
 
 # Configuration file for the Sphinx documentation builder.
 #
